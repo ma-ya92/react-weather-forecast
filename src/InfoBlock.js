6b885e1ef4545063362cea15fe52fe6fb207a40f
Mariia Yaroshenko <mashayaroshenko92@gmail.com>
2023-03-30 20:20:39 +0300

    Simplified "weather data" code

--- src/InfoBlock.js
+++ src/InfoBlock.js
@@ -4,24 +4,15 @@ import axios from "axios";
 import { useEffect } from "react";
 
 export default function InfoBlock(props) {
-  let weatherData = {
-    date: "Friday 21:02",
-    description: "Cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-  };
-
-  const [temp, setTemp] = useState(null);
-  const [humidity, setHumidity] = useState(null);
-  const [wind, setWind] = useState(null);
-  const [icon, setIcon] = useState(null);
+  const [weatherData, setWeatherData] = useState({});
 
   function showTemperature(response) {
-    setTemp(Math.round(response.data.main.temp));
-    setHumidity(response.data.main.humidity);
-    setWind(Math.round(response.data.wind.speed));
-    setIcon(
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
+    setWeatherData({
+      temp: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+    });
   }
 
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -44,9 +35,9 @@ export default function InfoBlock(props) {
               <li className="card-text" />
               {props.city}
               <li className="font-italic" />
-              {weatherData.date}
+              PUT HERE A DATE!
               <li>
-                <strong> {temp} </strong>
+                <strong> {weatherData.temp} </strong>
                 <span className="units">
                   <button href="#" className="link-button">
                     {" "}
@@ -58,13 +49,15 @@ export default function InfoBlock(props) {
             </ul>
           </div>
           <div className="col-4 mt-1" float-center>
-            <img src={icon} alt="" width="85px" />
+            <img src={weatherData.icon} alt="" width="85px" />
           </div>
           <div className="col-4">
             <ul className="list-unstyled">
               <br />
-              <li className="font-italic pt-2">Humidity: {humidity}%</li>
-              <li className="font-italic pb-2">Wind:{wind} m/h</li>
+              <li className="font-italic pt-2">
+                Humidity: {weatherData.humidity}%
+              </li>
+              <li className="font-italic pb-2">Wind:{weatherData.wind} m/h</li>
               <br />
               <li />
             </ul>
