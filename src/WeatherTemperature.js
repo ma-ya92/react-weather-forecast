368dd165994143fb614631142156639db0dd1662
Mariia Yaroshenko <mashayaroshenko92@gmail.com>
2023-03-31 23:53:25 +0300

    Fahrenheit convertion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,16 +1,50 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div сlassName="WeatherTemperature">
-      <span className="weatherIndex"> {props.celsius} </span>
-      <span className="units">
-        <button href="#" className="link-button">
-          {" "}
-          °C
-        </button>
-        |<button className="link-button"> °F</button>
-      </span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return Math.round((props.celsius * 9) / 5 + 32);
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div сlassName="WeatherTemperature">
+        <span className="weatherIndex"> {props.celsius} </span>
+        <span className="units">
+          <button href="#" className="link-button">
+            {" "}
+            °C
+          </button>
+          |
+          <button className="link-button" onClick={showFahrenheit}>
+            {" "}
+            °F{" "}
+          </button>
+        </span>
+      </div>
+    );
+  } else
+    return (
+      <div сlassName="WeatherTemperature">
+        <span className="weatherIndex"> {fahrenheit()} </span>
+        <span className="units">
+          <button href="#" className="link-button" onClick={showCelsius}>
+            {" "}
+            °C
+          </button>
+          |<button className="link-button"> °F </button>
+        </span>
+      </div>
+    );
 }
